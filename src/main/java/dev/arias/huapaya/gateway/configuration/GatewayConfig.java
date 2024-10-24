61d7410304c37971d779c29f7699626aabf1671d
Arias Huapaya Xavier Alexis <xavieralexisariashuapaya@hotmail.com>
2024-10-23 23:11:14 -0500

    Initial commit

---
+++ src/main/java/dev/arias/huapaya/gateway/configuration/GatewayConfig.java
@@ -0,0 +1,46 @@
+package dev.arias.huapaya.gateway.configuration;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Configuration
+public class GatewayConfig {
+
+        @Profile(value = "eureka-off")
+        @Bean
+        public RouteLocator RouteLocatorEurekaOff(RouteLocatorBuilder builder) {
+                return builder
+                                .routes()
+                                .route(route -> route.path("/api/maintenance/**")
+                                                .uri("http://localhost:8080"))
+                                .build();
+        }
+
+        @Profile(value = "eureka-on")
+        @Bean
+        public RouteLocator RouteLocatorEurekaOn(RouteLocatorBuilder builder) {
+                return builder
+                                .routes()
+                                .route(route -> route.path("/proxy-maintenance/**")
+                                                .uri("lb://ms-maintenance-proxy"))
+                                .route(route -> route.path("/proxy-report-maintenance/**")
+                                                .uri("lb://ms-maintenance-report-proxy"))
+                                // .route(route -> route.path("/proxy-sale/**")
+                                //                 .uri("lb://digital-peru-proxy-sale"))
+
+                                .route(route -> route.path("/api/maintenance/**")
+                                                .uri("lb://ms-maintenance"))
+                                .route(route -> route.path("/api/report/maintenance/**")
+                                                .uri("lb://ms-maintenance-report"))
+                                // .route(route -> route.path("/api/sale/**")
+                                //                 .uri("lb://digital-peru-sale"))
+                                .build();
+        }
+
+}
