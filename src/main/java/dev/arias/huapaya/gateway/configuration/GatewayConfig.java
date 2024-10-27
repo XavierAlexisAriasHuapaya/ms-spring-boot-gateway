5ec8dff33621bee1796b99d874c36face949341d
Arias Huapaya Xavier Alexis <xavieralexisariashuapaya@hotmail.com>
2024-10-26 20:41:08 -0500

    add security

--- src/main/java/dev/arias/huapaya/gateway/configuration/GatewayConfig.java
+++ src/main/java/dev/arias/huapaya/gateway/configuration/GatewayConfig.java
@@ -1,16 +1,21 @@
 package dev.arias.huapaya.gateway.configuration;
 
+import java.util.Set;
+
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import dev.arias.huapaya.gateway.filters.AuthenticationFilter;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @Configuration
 public class GatewayConfig {
+
+        private final AuthenticationFilter authenticationFilter;
 
         @Profile(value = "eureka-off")
         @Bean
@@ -28,7 +33,16 @@ public class GatewayConfig {
                 return builder
                                 .routes()
                                 .route(route -> route.path("/proxy-maintenance/**")
-                                                .uri("lb://ms-maintenance-proxy"))
+                                                .filters(filter -> {
+                                                        filter.filter(this.authenticationFilter);
+                                                        filter.circuitBreaker(config -> config
+                                                                        .setName("ms-gateway")
+                                                                        .setStatusCodes(Set.of("500", "400"))
+                                                                        .setFallbackUri("http://localhost:8787/fallback/fallback"));
+                                                        
+                                                        return filter;
+                                                }).uri("lb://ms-maintenance-proxy"))
+
                                 .route(route -> route.path("/proxy-report-maintenance/**")
                                                 .uri("lb://ms-maintenance-report-proxy"))
 
@@ -44,7 +58,15 @@ public class GatewayConfig {
 
                                 .route(route -> route.path("/api/sale/**")
                                                 .uri("lb://ms-sale"))
+
+                                .route(route -> route.path("/api/security/**")
+                                                .uri("lb://ms-authentication"))
+
+                                .route(route -> route.path("/fallback/**")
+                                                .uri("lb://ms-fallback"))
+
                                 .build();
+
         }
 
 }
---
+++ src/main/java/dev/arias/huapaya/gateway/dto/JwtDto.java
@@ -0,0 +1,16 @@
+package dev.arias.huapaya.gateway.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JwtDto {
+
+    private String accessToken;
+    
+}
